e039173e004591efd14aa3939cd0d3372b7bd64d
olyesung <84299012+olyesung@users.noreply.github.com>
2021-07-11 13:39:56 +0900

    6

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,14 @@
 const clock = document.querySelector("h2#clock");
 const dateMdy = document.getElementById("date-mdy")
-const date = new Date();
 
 
 function getClock() {
+    const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hours}:${minutes}:${seconds}`
-}
-function getDate() {
+
     const week = new Array('일', '월', '화', '수', '목', '금', '토');
     const day  = week[date.getDay()];
     const mm = date.getMonth()+1;
@@ -20,6 +19,3 @@ function getDate() {
 
 getClock();
 setInterval(getClock, 1000);
-
-getDate();
-setInterval(getDate, 10000);
